5b1b28c3e795730e360785b65d07f51263ea4272
Vittorio Carlo <vittoriocarlotoledo@gmail.com>
2019-07-02 10:11:49 -0500

    Proyecto finalizado

---
+++ Js/app.js
@@ -0,0 +1,12 @@
+const $burguerNav = document.getElementById('burguer-nav');
+const $navSecond = document.querySelector('.nav-second');
+
+$burguerNav.addEventListener('click', burguerButton);
+
+function burguerButton() {
+	if ($navSecond.classList.contains('is-active')) {
+			$navSecond.classList.remove('is-active');
+	} else {
+		$navSecond.classList.add('is-active');
+	}
+}
